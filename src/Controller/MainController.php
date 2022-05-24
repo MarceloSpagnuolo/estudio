0b75d4992594c0de78d89ec8e533bb891ce84ff1
MarceloSpagnuolo <lu4huf@gmail.com>
2022-05-24 16:43:58 -0300

    working in twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $pathName = "Home";
+
+        return $this->render('main/index.html.twig', [
+            'pathName' => $pathName,
+        ]);
+    }
+
+    #[Route('/page1', name: 'page1')]
+    public function page1(): Response
+    {
+        $pathName = "Page 1";
+
+        return $this->render('main/page1.html.twig', [
+            'pathName' => $pathName,
+        ]);
+    }
+
+    #[Route('/page2', name: 'page2')]
+    public function page2(): Response
+    {
+        $pathName = "Page 2";
+
+        return $this->render('main/page2.html.twig', [
+            'pathName' => $pathName,
+        ]);
+    }
+
+    #[Route('/page3', name: 'page3')]
+    public function page3(): Response
+    {
+        $pathName = "Page 3";
+
+        return $this->render('main/page3.html.twig', [
+            'pathName' => $pathName,
+        ]);
+    }
+
+
+    #[Route("/render-header", name: "renderHeader")]
+    public function renderHeader()
+    {
+        $arrayMenu = array(
+            'page1', 'page2', 'page3',
+        );
+
+        return $this->render('menuheader.html.twig', [
+            'arrayMenu' => $arrayMenu,
+        ]);
+    }
+}
